7252043725d25762f92feef771e3a0c869b82f98
Tiehuis <impressionistic@hotmail.com>
2013-09-03 16:55:12 +1200

    Changing to adhere to code standard

--- FootballFumble/src/nz/ac/waikato/cs/comp204/spartans/footballfumble/FixedImage.java
+++ FootballFumble/src/nz/ac/waikato/cs/comp204/spartans/footballfumble/FixedImage.java
@@ -2,14 +2,24 @@ package nz.ac.waikato.cs.comp204.spartans.footballfumble;
 
 import android.graphics.Bitmap;
 
+/**
+ *  A simple FixedImage class.
+ * 
+ * Holds a constant for the mass of the fixedImage which is
+ * passed to the superclass upon instantiation of a new FixedImage
+ * */
 public class FixedImage extends Sprite{
 	
-	// Fields for the ball...
+	private int FIXEDIMAGE_MASS = 5;
 	
+	/**
+	* Initialises the fixedImage with a mass of 5.
+ 	* @param initX the initial x position
+ 	* @param initY the initial y position
+ 	* @param drawView the view the fixedImage will exist
+ 	* @param bitmap the bitmap to draw the fixedImage on
+ 	*/
 	public FixedImage(int initX, int initY, DrawView drawView, Bitmap bitmap) {
-		
-		super(initX, initY, 0, 0, drawView, bitmap, 5);
+		super(initX, initY, 0, 0, drawView, bitmap, FIXEDIMAGE_MASS);
 	}
-	
-	// Methods for the ball...
 }
